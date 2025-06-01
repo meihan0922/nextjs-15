b80fd940f00d152e80b801116ac8108b7b898b00
Mei Huang <meihen0922@gmail.com>
2025-06-01 17:52:40 +0800

    Implement Question Card

--- constants/routes.ts
+++ constants/routes.ts
@@ -3,6 +3,7 @@ const ROUTES = {
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
   PROFILE: (id: string) => `/profile/${id}`,
+  QUESTION: (id: string) => `/question/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
   ASK_QUESTION: "/ask-question",
 };
