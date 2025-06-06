94ace4e621d07c142ccc0a3fc8264bb34cb5ca5a
Mei Huang <meihen0922@gmail.com>
2025-06-06 22:23:54 +0800

    Implement Account Model

---
+++ database/account.model.ts
@@ -0,0 +1,27 @@
+import { model, models, Schema, Types } from "mongoose";
+
+// 單個使用者可以有多個帳戶
+export interface IAccount {
+  userId: Types.ObjectId; // 對應到 User Model
+  name: string;
+  image?: string;
+  password?: string; // 傳統的帳密登入，對於某些身份驗證不是必需的
+  provider: string; // google, fb,  github, etc.
+  providerAccountId: string;
+}
+
+const AccountSchema = new Schema<IAccount>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", unique: true },
+    name: { type: String, required: true },
+    image: { type: String },
+    password: { type: String },
+    provider: { type: String, required: true },
+    providerAccountId: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+const Account = models?.account || model<IAccount>("Account", AccountSchema);
+
+export default Account;
