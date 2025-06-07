ee9d221d7538350f504f1d40a1a0c948f5265c51
Mei Huang <meihen0922@gmail.com>
2025-06-07 17:05:45 +0800

    Implement Question model

--- database/question.model.ts
+++ database/question.model.ts
@@ -3,9 +3,9 @@ import { model, models, Schema, Types } from "mongoose";
 export interface IQuestion {
   title: string;
   content: string;
-  tags: Types.ObjectId[]; // 對應到 Tag Model
+  tags: Types.ObjectId[]; // 對應到 Tag Model，一個問題對應到多個標籤
   views: number; // 瀏覽數
-  answers: number; // 回應數
+  answers: number; // 是一對多的關係，所以不用存成陣列
   upvotes: number;
   downvotes: number;
   author: Types.ObjectId; // 對應到 User Model，一個問題對應一個作者
